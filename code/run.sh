8d664fcca2ac2d034b738e8a484e84fab67e7929
eric-ubuntu-wsl <ericwong_1992@outlook.com>
2020-06-04 16:22:21 +0800

    modify run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,1 +1,3 @@
-./fastwcds --i n1000_m10000_s1.dimw --t 30 --s 1 --f0 100 --f1 100000 --it 2 --ct 10
+rm fastwcds
+g++ -O3 -std=c++11 fastds-pure.cpp -o fastwcds
+./fastwcds --i ../Instances/n1000_m10000_s1.dimw --t 30 --s 1 --f0 100 --f1 100000 --it 2 --ct 10
