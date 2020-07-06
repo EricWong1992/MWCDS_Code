2216aac6060668bd8054668e358423d68fa89780
eric-ubuntu-wsl <ericwong_1992@outlook.com>
2020-07-06 14:31:22 +0800

    增加build.sh

---
+++ code/build.sh
@@ -0,0 +1,2 @@
+rm fastwcds
+g++ -O3 -std=c++11 fastds-pure.cpp -o fastwcds
--- code/run.sh
+++ code/run.sh
@@ -1,3 +1,2 @@
-rm fastwcds
-g++ -O3 -std=c++11 fastds-pure.cpp -o fastwcds
+./build.sh
 ./fastwcds --i ../Instances/n1000_m10000_s1.dimw --t 30 --s 1 --f0 100 --f1 100000 --it 2 --ct 10
